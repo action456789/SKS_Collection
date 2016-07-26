c2151dd462325e9e2a6a8cba5131275206eaa3e7
kesen <kesen@fun.tv>
2016-07-26 11:08:17 +0800

    add scan line for QRCodeViewController

--- SKS_Collection/MainController/QRCoder/QRCodeViewController.m
+++ SKS_Collection/MainController/QRCoder/QRCodeViewController.m
@@ -10,11 +10,16 @@
 #import "QRCodeView.h"
 #import "UIDevice+KS.h"
 #import "QRCodeMaskView.h"
+#import "UIView+Frame.h"
+
+#define kViewfinderWH SCREEN_WIDTH * 0.8
+#define kPadding 10
 
 @interface QRCodeViewController()<QRCodeViewDelegate>
 {
     QRCodeView * _qrCodeView;
-    UIImageView *_scannerView;
+    UIImageView *_scannerLineView;
+    UIImageView *_viewfinder;
 }
 @end
 
@@ -25,6 +30,8 @@
     [super viewDidLoad];
 
     [self addSubViews];
+    
+    [self startScannerLineAnimation];
 }
 
 - (void)addSubViews
@@ -36,26 +43,42 @@
     _qrCodeView = qrCodeView;
     
     UIImageView *viewfinder = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"Image-1"]];
-    [self.view addSubview:viewfinder];
-    viewfinder.bounds = CGRectMake(0, 0, SCREEN_WIDTH * 0.8, SCREEN_WIDTH * 0.8);
+    viewfinder.bounds = CGRectMake(0, 0, kViewfinderWH, SCREEN_WIDTH * 0.8);
     viewfinder.center = self.view.center;
+    _viewfinder = viewfinder;
+    [self.view addSubview:viewfinder];
+    
+    UIImageView *scanLine = [[UIImageView alloc] initWithImage: [UIImage imageNamed:@"scanLine"]];
+    scanLine.width = kViewfinderWH;
+    scanLine.height = 13;
+    scanLine.center = viewfinder.center;
+    _scannerLineView = scanLine;
+    [self.view addSubview:scanLine];
     
     QRCodeMaskView *maskView = [[QRCodeMaskView alloc] initWithFrame:self.view.bounds];
     maskView.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.5f];
     maskView.cutOutRect = viewfinder.frame;
     [self.view addSubview:maskView];
-    
-    UIImageView *scanLine = [[UIImageView alloc] initWithImage: [UIImage imageNamed:@"scanLine"]];
-    CGFloat scanLineX = viewfinder.frame.origin.x;
-    CGFloat scanLineY = viewfinder.frame.origin.y;
-    CGFloat scanLineW = viewfinder.frame.size.width;
-    scanLine.frame = CGRectMake(0, 10, 100, 3);
-    [self.view addSubview:scanLine];
 }
 
-- (void)scannerViewStartAnimate
+- (void)startScannerLineAnimation
 {
-
+    CAKeyframeAnimation *animation = [CAKeyframeAnimation animation];
+    animation.keyPath = @"position";
+    animation.duration = 2.0;
+    animation.repeatCount = MAXFLOAT;
+    animation.removedOnCompletion = NO;
+    animation.fillMode = kCAFillModeForwards;
+    
+    CGFloat x = _scannerLineView.center.x;
+    CGFloat y = _scannerLineView.center.y;
+    NSValue *v1 = [NSValue valueWithCGPoint:CGPointMake(x, y - kViewfinderWH * 0.5)];
+    NSValue *v2 = [NSValue valueWithCGPoint:CGPointMake(x, y + kViewfinderWH * 0.5)];
+    NSValue *v3 = [NSValue valueWithCGPoint:CGPointMake(x, y - kViewfinderWH * 0.5)];
+    
+    animation.values = @[v1,v2,v3];
+    
+    [_scannerLineView.layer addAnimation:animation forKey:nil];
 }
 
 - (void)QRCodeView:(QRCodeView *)view scanedResultString:(NSString *)result
