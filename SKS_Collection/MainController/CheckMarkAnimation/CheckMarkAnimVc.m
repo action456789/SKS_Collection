0473fa89558f23279c50e3880e9905511b5c7e37
kesen <kesen@fun.tv>
2016-08-10 17:18:59 +0800

    add disign data of checkmark animation

--- SKS_Collection/MainController/CheckMarkAnimation/CheckMarkAnimVc.m
+++ SKS_Collection/MainController/CheckMarkAnimation/CheckMarkAnimVc.m
@@ -18,7 +18,6 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
     KSLayAnimationView *checkView = [KSLayAnimationView layAnimationView];
     [checkView setCenter:self.view.center];
     checkView.backgroundColor = [UIColor blackColor];
@@ -26,18 +25,16 @@
     [self.view addSubview:checkView];
     
     KSLayAnimationView *crossView = [KSLayAnimationView layAnimationView];
-
     crossView.x = checkView.x;
     crossView.y = checkView.y + 50;
     crossView.backgroundColor = [UIColor blackColor];
     _crossView = crossView;
     [self.view addSubview:crossView];
-}
-
-- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
-{
-    [_checkView showWithDuration:2 afterDelay:0 type:KSLayAnimationTypeCheckMark];
-    [_crossView showWithDuration:2 afterDelay:0 type:KSLayAnimationTypeCross];
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5f * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [_checkView showWithDuration:1 afterDelay:0 type:KSLayAnimationTypeCheckMark];
+        [_crossView showWithDuration:1 afterDelay:0 type:KSLayAnimationTypeCross];
+    });
 }
 
 @end
