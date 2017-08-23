ff315fe66f0baa371309ff405a58792394d8d368
superkesen <superkesen@gmail.com>
2017-08-23 17:55:25 +0800

    增加哈希算法

---
+++ SKS_Collection/Classes/Category/NSString/NSData+Hash.h
@@ -0,0 +1,13 @@
+//
+//  NSData+Hash.h
+//  SKS_Collection
+//
+//  Created by sen.ke on 2017/8/23.
+//  Copyright © 2017年 SenKe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSData (Hash)
+
+@end
