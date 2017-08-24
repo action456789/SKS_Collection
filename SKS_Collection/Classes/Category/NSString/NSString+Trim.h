d5243f96eed62515768eabdb7ead2f15c85ede2c
superkesen <superkesen@gmail.com>
2017-08-24 15:12:58 +0800

    update NSString+Trim

--- SKS_Collection/Classes/Category/NSString/NSString+Trim.h
+++ SKS_Collection/Classes/Category/NSString/NSString+Trim.h
@@ -6,7 +6,7 @@
 //  Copyright © 2017年 SenKe. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 @interface NSString (Trim)
 
@@ -14,5 +14,64 @@
  *  去除字符串首尾空格
  */
 - (NSString *)kk_trim;
+
+/**
+ Add scale modifier to the file name (without path extension),
+ From @"name" to @"name@2x".
+ 
+ e.g.
+ <table>
+ <tr><th>Before     </th><th>After(scale:2)</th></tr>
+ <tr><td>"icon"     </td><td>"icon@2x"     </td></tr>
+ <tr><td>"icon "    </td><td>"icon @2x"    </td></tr>
+ <tr><td>"icon.top" </td><td>"icon.top@2x" </td></tr>
+ <tr><td>"/p/name"  </td><td>"/p/name@2x"  </td></tr>
+ <tr><td>"/path/"   </td><td>"/path/"      </td></tr>
+ </table>
+ 
+ @param scale Resource scale.
+ @return String by add scale modifier, or just return if it's not end with file name.
+ */
+- (NSString *)kk_stringByAppendingNameScale:(CGFloat)scale;
+
+/**
+ Add scale modifier to the file path (with path extension),
+ From @"name.png" to @"name@2x.png".
+ 
+ e.g.
+ <table>
+ <tr><th>Before     </th><th>After(scale:2)</th></tr>
+ <tr><td>"icon.png" </td><td>"icon@2x.png" </td></tr>
+ <tr><td>"icon..png"</td><td>"icon.@2x.png"</td></tr>
+ <tr><td>"icon"     </td><td>"icon@2x"     </td></tr>
+ <tr><td>"icon "    </td><td>"icon @2x"    </td></tr>
+ <tr><td>"icon."    </td><td>"icon.@2x"    </td></tr>
+ <tr><td>"/p/name"  </td><td>"/p/name@2x"  </td></tr>
+ <tr><td>"/path/"   </td><td>"/path/"      </td></tr>
+ </table>
+ 
+ @param scale Resource scale.
+ @return String by add scale modifier, or just return if it's not end with file name.
+ */
+- (NSString *)kk_stringByAppendingPathScale:(CGFloat)scale;
+
+/**
+ nil, @"", @"  ", @"\n" will Returns NO; otherwise Returns YES.
+ */
+- (BOOL)kk_isNotBlank;
+
+/**
+ Returns YES if the target string is contained within the receiver.
+ @param string A string to test the the receiver.
+ 
+ @discussion Apple has implemented this method in iOS8.
+ */
+- (BOOL)kk_containsString:(NSString *)string;
+
+/**
+ Returns YES if the target CharacterSet is contained within the receiver.
+ @param set  A character set to test the the receiver.
+ */
+- (BOOL)kk_containsCharacterSet:(NSCharacterSet *)set;
 
 @end
--- SKS_Collection/Classes/Category/NSString/NSString+Trim.m
+++ SKS_Collection/Classes/Category/NSString/NSString+Trim.m
@@ -14,4 +14,39 @@
     return [self stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
 }
 
+- (NSString *)kk_stringByAppendingNameScale:(CGFloat)scale {
+    if (fabs(scale - 1) <= __FLT_EPSILON__ || self.length == 0 || [self hasSuffix:@"/"]) return self.copy;
+    return [self stringByAppendingFormat:@"@%@x", @(scale)];
+}
+
+- (NSString *)kk_stringByAppendingPathScale:(CGFloat)scale {
+    if (fabs(scale - 1) <= __FLT_EPSILON__ || self.length == 0 || [self hasSuffix:@"/"]) return self.copy;
+    NSString *ext = self.pathExtension;
+    NSRange extRange = NSMakeRange(self.length - ext.length, 0);
+    if (ext.length > 0) extRange.location -= 1;
+    NSString *scaleStr = [NSString stringWithFormat:@"@%@x", @(scale)];
+    return [self stringByReplacingCharactersInRange:extRange withString:scaleStr];
+}
+
+- (BOOL)kk_isNotBlank {
+    NSCharacterSet *blank = [NSCharacterSet whitespaceAndNewlineCharacterSet];
+    for (NSInteger i = 0; i < self.length; ++i) {
+        unichar c = [self characterAtIndex:i];
+        if (![blank characterIsMember:c]) {
+            return YES;
+        }
+    }
+    return NO;
+}
+
+- (BOOL)kk_containsString:(NSString *)string {
+    if (string == nil) return NO;
+    return [self rangeOfString:string].location != NSNotFound;
+}
+
+- (BOOL)kk_containsCharacterSet:(NSCharacterSet *)set {
+    if (set == nil) return NO;
+    return [self rangeOfCharacterFromSet:set].location != NSNotFound;
+}
+
 @end
