3d6f3d6a9f6bec0735ee669d585924ab9c04a7ac
superkesen <superkesen@gmail.com>
2017-08-24 18:19:20 +0800

    add NSDate+String

---
+++ SKS_Collection/Classes/Category/NSDate/NSDate+String.h
@@ -0,0 +1,97 @@
+//
+//  NSDate+String.h
+//  SKS_Collection
+//
+//  Created by sen.ke on 2017/8/24.
+//  Copyright © 2017年 SenKe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSDate (String)
+
+#pragma mark - Date Format
+///=============================================================================
+/// @name Date Format
+///=============================================================================
+
+/**
+ Returns a formatted string representing this date.
+ see http://www.unicode.org/reports/tr35/tr35-31/tr35-dates.html#Date_Format_Patterns
+ for format description.
+ 
+ @param format   String representing the desired date format.
+ e.g. @"yyyy-MM-dd HH:mm:ss"
+ 
+ @return NSString representing the formatted date string.
+ */
+- (nullable NSString *)kk_stringWithFormat:(NSString *)format;
+
+/**
+ Returns a formatted string representing this date.
+ see http://www.unicode.org/reports/tr35/tr35-31/tr35-dates.html#Date_Format_Patterns
+ for format description.
+ 
+ @param format    String representing the desired date format.
+ e.g. @"yyyy-MM-dd HH:mm:ss"
+ 
+ @param timeZone  Desired time zone.
+ 
+ @param locale    Desired locale.
+ 
+ @return NSString representing the formatted date string.
+ */
+- (nullable NSString *)kk_stringWithFormat:(NSString *)format
+                               timeZone:(nullable NSTimeZone *)timeZone
+                                 locale:(nullable NSLocale *)locale;
+
+/**
+ Returns a string representing this date in ISO8601 format.
+ e.g. "2010-07-09T16:13:30+12:00"
+ 
+ @return NSString representing the formatted date string in ISO8601.
+ */
+- (nullable NSString *)kk_stringWithISOFormat;
+
+/**
+ Returns a date parsed from given string interpreted using the format.
+ 
+ @param dateString The string to parse.
+ @param format     The string's date format.
+ 
+ @return A date representation of string interpreted using the format.
+ If can not parse the string, returns nil.
+ */
++ (nullable NSDate *)kk_dateWithString:(NSString *)dateString format:(NSString *)format;
+
+/**
+ Returns a date parsed from given string interpreted using the format.
+ 
+ @param dateString The string to parse.
+ @param format     The string's date format.
+ @param timeZone   The time zone, can be nil.
+ @param locale     The locale, can be nil.
+ 
+ @return A date representation of string interpreted using the format.
+ If can not parse the string, returns nil.
+ */
++ (nullable NSDate *)kk_dateWithString:(NSString *)dateString
+                             format:(NSString *)format
+                           timeZone:(nullable NSTimeZone *)timeZone
+                             locale:(nullable NSLocale *)locale;
+
+/**
+ Returns a date parsed from given string interpreted using the ISO8601 format.
+ 
+ @param dateString The date string in ISO8601 format. e.g. "2010-07-09T16:13:30+12:00"
+ 
+ @return A date representation of string interpreted using the format.
+ If can not parse the string, returns nil.
+ */
++ (nullable NSDate *)kk_dateWithISOFormatString:(NSString *)dateString;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SKS_Collection/Classes/Category/NSDate/NSDate+String.m
@@ -0,0 +1,64 @@
+//
+//  NSDate+String.m
+//  SKS_Collection
+//
+//  Created by sen.ke on 2017/8/24.
+//  Copyright © 2017年 SenKe. All rights reserved.
+//
+
+#import "NSDate+String.h"
+
+@implementation NSDate (String)
+
+- (NSString *)kk_stringWithFormat:(NSString *)format {
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    [formatter setDateFormat:format];
+    [formatter setLocale:[NSLocale currentLocale]];
+    return [formatter stringFromDate:self];
+}
+
+- (NSString *)kk_stringWithFormat:(NSString *)format timeZone:(NSTimeZone *)timeZone locale:(NSLocale *)locale {
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    [formatter setDateFormat:format];
+    if (timeZone) [formatter setTimeZone:timeZone];
+    if (locale) [formatter setLocale:locale];
+    return [formatter stringFromDate:self];
+}
+
+- (NSString *)kk_stringWithISOFormat {
+    static NSDateFormatter *formatter = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        formatter = [[NSDateFormatter alloc] init];
+        formatter.locale = [[NSLocale alloc] initWithLocaleIdentifier:@"en_US_POSIX"];
+        formatter.dateFormat = @"yyyy-MM-dd'T'HH:mm:ssZ";
+    });
+    return [formatter stringFromDate:self];
+}
+
++ (NSDate *)kk_dateWithString:(NSString *)dateString format:(NSString *)format {
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    [formatter setDateFormat:format];
+    return [formatter dateFromString:dateString];
+}
+
++ (NSDate *)kk_dateWithString:(NSString *)dateString format:(NSString *)format timeZone:(NSTimeZone *)timeZone locale:(NSLocale *)locale {
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    [formatter setDateFormat:format];
+    if (timeZone) [formatter setTimeZone:timeZone];
+    if (locale) [formatter setLocale:locale];
+    return [formatter dateFromString:dateString];
+}
+
++ (NSDate *)kk_dateWithISOFormatString:(NSString *)dateString {
+    static NSDateFormatter *formatter = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        formatter = [[NSDateFormatter alloc] init];
+        formatter.locale = [[NSLocale alloc] initWithLocaleIdentifier:@"en_US_POSIX"];
+        formatter.dateFormat = @"yyyy-MM-dd'T'HH:mm:ssZ";
+    });
+    return [formatter dateFromString:dateString];
+}
+
+@end
