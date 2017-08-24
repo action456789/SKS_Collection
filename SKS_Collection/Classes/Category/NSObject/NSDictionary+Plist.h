992e6fa9c6ce549c5062c6e52441d613a00d9652
superkesen <superkesen@gmail.com>
2017-08-24 16:26:33 +0800

    add NSDictionary+Plist

---
+++ SKS_Collection/Classes/Category/NSObject/NSDictionary+Plist.h
@@ -0,0 +1,71 @@
+//
+//  NSDictionary+Plist.h
+//  SKS_Collection
+//
+//  Created by sen.ke on 2017/8/24.
+//  Copyright © 2017年 SenKe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDictionary (Plist)
+
+/**
+ Creates and returns a dictionary from a specified property list data.
+ 
+ @param plist   A property list data whose root object is a dictionary.
+ @return A new dictionary created from the binary plist data, or nil if an error occurs.
+ */
++ (nullable NSDictionary *)kk_dictionaryWithPlistData:(NSData *_Nullable)plist;
+
+/**
+ Creates and returns a dictionary from a specified property list xml string.
+ 
+ @param plist   A property list xml string whose root object is a dictionary.
+ @return A new dictionary created from the plist string, or nil if an error occurs.
+ 
+ @discussion Apple has implemented this method, but did not make it public.
+ */
++ (nullable NSDictionary *)kk_dictionaryWithPlistString:(NSString *_Nullable)plist;
+
+/**
+ Serialize the dictionary to a binary property list data.
+ 
+ @return A binary plist data, or nil if an error occurs.
+ 
+ @discussion Apple has implemented this method, but did not make it public.
+ */
+- (nullable NSData *)kk_plistData;
+
+/**
+ Serialize the dictionary to a xml property list string.
+ 
+ @return A plist xml string, or nil if an error occurs.
+ */
+- (nullable NSString *)kk_plistString;
+
+@end
+
+
+
+@interface NSMutableDictionary (Plist)
+
+/**
+ Creates and returns a dictionary from a specified property list data.
+ 
+ @param plist   A property list data whose root object is a dictionary.
+ @return A new dictionary created from the binary plist data, or nil if an error occurs.
+ 
+ @discussion Apple has implemented this method, but did not make it public.
+ */
++ (nullable NSMutableDictionary *)kk_dictionaryWithPlistData:(NSData *_Nullable)plist;
+
+/**
+ Creates and returns a dictionary from a specified property list xml string.
+ 
+ @param plist   A property list xml string whose root object is a dictionary.
+ @return A new dictionary created from the plist string, or nil if an error occurs.
+ */
++ (nullable NSMutableDictionary *)kk_dictionaryWithPlistString:(NSString *_Nullable)plist;
+
+@end
---
+++ SKS_Collection/Classes/Category/NSObject/NSDictionary+Plist.m
@@ -0,0 +1,55 @@
+//
+//  NSDictionary+Plist.m
+//  SKS_Collection
+//
+//  Created by sen.ke on 2017/8/24.
+//  Copyright © 2017年 SenKe. All rights reserved.
+//
+
+#import "NSDictionary+Plist.h"
+
+@implementation NSDictionary (Plist)
+
++ (NSDictionary *)kk_dictionaryWithPlistData:(NSData *)plist {
+    if (!plist) return nil;
+    NSDictionary *dictionary = [NSPropertyListSerialization propertyListWithData:plist options:NSPropertyListImmutable format:NULL error:NULL];
+    if ([dictionary isKindOfClass:[NSDictionary class]]) return dictionary;
+    return nil;
+}
+
++ (NSDictionary *)kk_dictionaryWithPlistString:(NSString *)plist {
+    if (!plist) return nil;
+    NSData *data = [plist dataUsingEncoding:NSUTF8StringEncoding];
+    return [self kk_dictionaryWithPlistData:data];
+}
+
+- (NSData *)kk_plistData {
+    return [NSPropertyListSerialization dataWithPropertyList:self format:NSPropertyListBinaryFormat_v1_0 options:kNilOptions error:NULL];
+}
+
+- (NSString *)kk_plistString {
+    NSData *xmlData = [NSPropertyListSerialization dataWithPropertyList:self format:NSPropertyListXMLFormat_v1_0 options:kNilOptions error:NULL];
+    if (xmlData) return [[NSString alloc] initWithData:xmlData encoding:kCFStringEncodingUTF8];
+    return nil;
+}
+
+@end
+
+
+
+@implementation NSMutableDictionary (Plist)
+
++ (NSMutableDictionary *)dictionaryWithPlistData:(NSData *)plist {
+    if (!plist) return nil;
+    NSMutableDictionary *dictionary = [NSPropertyListSerialization propertyListWithData:plist options:NSPropertyListMutableContainersAndLeaves format:NULL error:NULL];
+    if ([dictionary isKindOfClass:[NSMutableDictionary class]]) return dictionary;
+    return nil;
+}
+
++ (NSMutableDictionary *)dictionaryWithPlistString:(NSString *)plist {
+    if (!plist) return nil;
+    NSData *data = [plist dataUsingEncoding:NSUTF8StringEncoding];
+    return [self dictionaryWithPlistData:data];
+}
+
+@end
