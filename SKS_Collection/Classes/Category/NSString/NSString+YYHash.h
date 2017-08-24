8020e862dfe07cc38c63d93df630e817c33c7f00
superkesen <superkesen@gmail.com>
2017-08-24 09:41:01 +0800

    update file structures

---
+++ SKS_Collection/Classes/Category/NSString/NSString+YYHash.h
@@ -0,0 +1,13 @@
+//
+//  NSString+YYHash.h
+//  SKS_Collection
+//
+//  Created by sen.ke on 2017/8/24.
+//  Copyright © 2017年 SenKe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (YYHash)
+
+@end
---
+++ SKS_Collection/Classes/Category/NSString/NSString+YYHash.m
@@ -0,0 +1,13 @@
+//
+//  NSString+YYHash.m
+//  SKS_Collection
+//
+//  Created by sen.ke on 2017/8/24.
+//  Copyright © 2017年 SenKe. All rights reserved.
+//
+
+#import "NSString+YYHash.h"
+
+@implementation NSString (YYHash)
+
+@end
