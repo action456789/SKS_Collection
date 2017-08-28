c850a3b74fc6856666933269b156871c2ecdee26
superkesen <superkesen@gmail.com>
2017-08-28 14:35:13 +0800

    add UIDevice+CPU

--- SKS_Collection/Classes/Category/UIKit/UIDevice/UIDevice+CPU.h
+++ SKS_Collection/Classes/Category/UIKit/UIDevice/UIDevice+CPU.h
@@ -10,4 +10,18 @@
 
 @interface UIDevice (CPU)
 
+#pragma mark - CPU Information
+///=============================================================================
+/// @name CPU Information
+///=============================================================================
+
+/// Avaliable CPU processor count.
+@property (nonatomic, readonly) NSUInteger kk_cpuCount;
+
+/// Current CPU usage, 1.0 means 100%. (-1 when error occurs)
+@property (nonatomic, readonly) float kk_cpuUsage;
+
+/// Current CPU usage per processor (array of NSNumber), 1.0 means 100%. (nil when error occurs)
+@property (nullable, nonatomic, readonly) NSArray<NSNumber *> *kk_cpuUsagePerProcessor;
+
 @end
--- SKS_Collection/MainController/AppleSystemService/AppleSystemServiceViewController.m
+++ SKS_Collection/MainController/AppleSystemService/AppleSystemServiceViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "AppleSystemServiceViewController.h"
+#import "UIDevice+CPU.h"
+#import "UIDevice+DiskSpace.h"
 #import "UIDevice+DeviceInfo.h"
 
 @implementation AppleSystemServiceViewController
@@ -14,6 +16,23 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    NSLog(@"磁盘信息");
+    NSLog(@"kk_diskSpace: %lld", [UIDevice currentDevice].kk_diskSpace);
+    NSLog(@"kk_diskSpaceFree: %lld", [UIDevice currentDevice].kk_diskSpaceFree);
+    NSLog(@"kk_diskSpaceUsed: %lld", [UIDevice currentDevice].kk_diskSpaceUsed);
+    
+    NSLog(@"系统版本");
+    NSLog(@"kk_isiOS7Later: %d", [UIDevice kk_isiOS7Later]);
+    NSLog(@"kk_isiOS8Later: %d", [UIDevice kk_isiOS8Later]);
+    NSLog(@"kk_isiOS9Later: %d", [UIDevice kk_isiOS9Later]);
+    NSLog(@"kk_isiOS10Later: %d", [UIDevice kk_isiOS10Later]);
+    NSLog(@"kk_isiOS11Later: %d", [UIDevice kk_isiOS11Later]);
+    
+    NSLog(@"CPU信息");
+    NSLog(@"kk_cpuCount: %lu", (unsigned long)[[UIDevice currentDevice] kk_cpuCount]);
+    NSLog(@"kk_cpuUsage: %f", [[UIDevice currentDevice] kk_cpuUsage]);
+    NSLog(@"kk_cpuUsagePerProcessor: %@", [[UIDevice currentDevice] kk_cpuUsagePerProcessor]);
 }
 
 @end
